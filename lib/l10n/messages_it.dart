64928cee9cda9002bde6e9bdfcda9bde816fd91b
DenisStark <stark@mail.ru>
2019-01-31 22:56:46 +0400

    Add italian translation

---
+++ lib/l10n/messages_it.dart
@@ -0,0 +1,53 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a it locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+// ignore: unnecessary_new
+final messages = new MessageLookup();
+
+// ignore: unused_element
+final _keepAnalysisHappy = Intl.defaultLocale;
+
+// ignore: non_constant_identifier_names
+typedef MessageIfAbsent(String message_str, List args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  get localeName => 'it';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "addShelf" : MessageLookupByLibrary.simpleMessage("Aggiungi il tuo scaffale"),
+    "blockUser" : MessageLookupByLibrary.simpleMessage("Bloccare l’Utente abusivo"),
+    "chat" : MessageLookupByLibrary.simpleMessage("CHAT"),
+    "deleteShelf" : MessageLookupByLibrary.simpleMessage("Cancella questo scaffale"),
+    "introDone" : MessageLookupByLibrary.simpleMessage("FATTO"),
+    "introMeet" : MessageLookupByLibrary.simpleMessage("Incontrarsi"),
+    "introMeetHint" : MessageLookupByLibrary.simpleMessage("Contatta il proprietario dei libri che ti piacciono e fissa un appuntamento per averli."),
+    "introShoot" : MessageLookupByLibrary.simpleMessage("Fai foto"),
+    "introShootHint" : MessageLookupByLibrary.simpleMessage("Scatta una foto dei tuoi scaffali e condividili ai vicini e ai turisti. I tuoi libri attraggono il aderenti"),
+    "introSkip" : MessageLookupByLibrary.simpleMessage("SALTARE"),
+    "introSurf" : MessageLookupByLibrary.simpleMessage("Esplora"),
+    "introSurfHint" : MessageLookupByLibrary.simpleMessage("App mostra librerie in 200 km intorno a te in ordine di distanza. Ottieni l\'accesso a un\'ampia varietà di libri."),
+    "km" : MessageLookupByLibrary.simpleMessage(" km"),
+    "loading" : MessageLookupByLibrary.simpleMessage("Loading..."),
+    "loginAgree1" : MessageLookupByLibrary.simpleMessage("Cliccando su Registrami confermi di accettare  \n le "),
+    "loginAgree2" : MessageLookupByLibrary.simpleMessage("condizioni di utilizzo e l\'informativa "),
+    "loginAgree3" : MessageLookupByLibrary.simpleMessage("\nsul trattamento dei "),
+    "loginAgree4" : MessageLookupByLibrary.simpleMessage("dati personali "),
+    "logout" : MessageLookupByLibrary.simpleMessage("Logout"),
+    "messageOwner" : MessageLookupByLibrary.simpleMessage("Messaggio al proprietario"),
+    "notBooks" : MessageLookupByLibrary.simpleMessage("Ehi, questa non sembra un scaffale per me"),
+    "nothingToSend" : MessageLookupByLibrary.simpleMessage("Niente da inviare"),
+    "reportShelf" : MessageLookupByLibrary.simpleMessage("Segnala un contenuto discutibile"),
+    "reportedPhoto" : MessageLookupByLibrary.simpleMessage("Questa foto è segnalata come i contenuti discutibili."),
+    "seeLocation" : MessageLookupByLibrary.simpleMessage("Vedere la posizione"),
+    "shareShelf" : MessageLookupByLibrary.simpleMessage("Condividi il scaffale"),
+    "title" : MessageLookupByLibrary.simpleMessage("Biblosfere"),
+    "typeMsg" : MessageLookupByLibrary.simpleMessage("Scrivi il tuo messaggio..."),
+    "zoom" : MessageLookupByLibrary.simpleMessage("ZOOM")
+  };
+}
